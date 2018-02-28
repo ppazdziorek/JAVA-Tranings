f573a83f8f234302a069c1613bf20214b71a57f3
PRZEMEK-PC\Przemysław <p.pazdziorek@gmail.com>
2018-02-28 22:27:02 +0100

    new: spock tests added

--- java-basic-traning-8/src/main/java/pl/codecouple/Printer.java
+++ java-basic-traning-8/src/main/java/pl/codecouple/Printer.java
@@ -1,10 +1,15 @@
 package pl.codecouple;
 
+import org.apache.commons.lang3.StringUtils;
+
 class Printer {
 
     private StringBuilder stringBuilder;
 
     Printer(String text) {
+        if(StringUtils.isEmpty(text)){
+            throw new IllegalArgumentException();
+        }
         this.stringBuilder = new StringBuilder(text);
     }
 
@@ -12,4 +17,16 @@ class Printer {
         return stringBuilder.reverse().toString();
     }
 
+    public int getIndexOf(String p) {
+        return stringBuilder.indexOf(p);
+    }
+
+    public String removeChar(String p) {
+        int index = stringBuilder.indexOf(p);
+        return stringBuilder.delete(index,index+1).toString();
+    }
+
+    public int getLastIndexOf(String p) {
+        return stringBuilder.lastIndexOf(p);
+    }
 }
--- java-basic-traning-8/src/test/java/pl/codecouple/PrinterSpec.groovy
+++ java-basic-traning-8/src/test/java/pl/codecouple/PrinterSpec.groovy
@@ -15,19 +15,37 @@ class PrinterSpec extends Specification {
     }
 
     Should "return indexOf 'a' char from 'abc'"() {
-
+        given:
+            Printer printer = new Printer('abc')
+        when:
+            def result = printer.getIndexOf("a")
+        then:
+            result == 0
     }
 
     Should "delete 'a' from 'abc'"() {
-
+        given:
+            Printer printer = new Printer('abc')
+        when:
+            def result = printer.removeChar("a")
+        then:
+            result == 'bc'
     }
 
     Should "thrown IllegalArgumentException when given String in constructor is null or empty" () {
-
+        when:
+            new Printer("")
+        then:
+            thrown(IllegalArgumentException)
     }
 
     Should "return lastIndexOf 'c' char from 'abc'"() {
-
+        given:
+            Printer printer = new Printer('abc')
+        when:
+            def result = printer.getLastIndexOf("c")
+        then:
+            result == 2
     }
 
 }
