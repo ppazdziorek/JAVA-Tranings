04d41fcce4fcdc644a34a7f96c12e329a4fa474c
PRZEMEK-PC\Przemysław <p.pazdziorek@gmail.com>
2018-03-20 21:52:37 +0100

    my-rest-service-update

--- myRestService/src/main/java/hello/GreetingController.java
+++ myRestService/src/main/java/hello/GreetingController.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "Czesc, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greetings")
-    public Greetings greetings (@RequestParam(value="name", defaultValue = "World") String name){
+    public Greetings greetings (@RequestParam(value="name", defaultValue = "Swiecie") String name){
         return new Greetings(counter.incrementAndGet(),
                 String.format(template, name));
     }
